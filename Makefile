81ccfac29b7e09e76d0dadd11dc98bc8acb629ea
Siyuan Liu <3104959526@qq.com>
2025-11-04 11:19:46 +0800

    cursor version 1

--- Makefile
+++ Makefile
@@ -4,18 +4,17 @@ unit-tests:
 	@if [ -d build ]; then rm -rf build; fi
 	@mkdir -p build
 	@cd build && cmake ..
-	@cd build && $(MAKE) build-tests -j
-	@cd build && $(MAKE) fs -j
+	@cd build && $(MAKE) -j
 	@cd build && $(MAKE) build-tests -j
 	@cd build && $(MAKE) test -j
 
-inte-tests:
+stress:
 	@if [ -d build ]; then rm -rf build; fi
 	@mkdir -p build
 	@cd build && cmake ..
+	@cd build && $(MAKE) -j
 	@cd build && $(MAKE) build-tests -j
-	@cd build && $(MAKE) fs -j
-	@cd build && cd ../scripts/lab1 && ./integration_test.sh
+	@cd build && $(MAKE) run_concurrent_stress_test
 
 clean:
 	@rm -rf build
--- src/distributed/dataserver.cc
+++ src/distributed/dataserver.cc
@@ -57,34 +57,94 @@ DataServer::~DataServer() { server_.reset(); }
 // {Your code here}
 auto DataServer::read_data(block_id_t block_id, usize offset, usize len,
                            version_t version) -> std::vector<u8> {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  // Validate args
+  if (len == 0 || offset + len > block_allocator_->bm->block_size()) {
+    return {};
+  }
 
-  return {};
+  // Helper: compute where the version is stored
+  auto bm = block_allocator_->bm;
+  const usize versions_per_block = bm->block_size() / sizeof(version_t);
+  const block_id_t ver_block_id = (block_id / versions_per_block);
+  const usize ver_offset = (block_id % versions_per_block) * sizeof(version_t);
+
+  // Read version from version block and compare
+  std::vector<u8> ver_buf(bm->block_size());
+  bm->read_block(ver_block_id, ver_buf.data());
+  version_t stored_version =
+      *reinterpret_cast<version_t *>(ver_buf.data() + ver_offset);
+  if (stored_version != version) {
+    return {};
+  }
+
+  // Read requested bytes
+  std::vector<u8> whole(bm->block_size());
+  bm->read_block(block_id, whole.data());
+  return std::vector<u8>(whole.begin() + offset, whole.begin() + offset + len);
 }
 
 // {Your code here}
 auto DataServer::write_data(block_id_t block_id, usize offset,
                             std::vector<u8> &buffer) -> bool {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
-
-  return false;
+  if (buffer.empty())
+    return true;
+  auto bm = block_allocator_->bm;
+  if (offset + buffer.size() > bm->block_size()) {
+    return false;
+  }
+  auto res = bm->write_partial_block(block_id, buffer.data(), offset,
+                                     buffer.size());
+  return res.is_ok();
 }
 
 // {Your code here}
 auto DataServer::alloc_block() -> std::pair<block_id_t, version_t> {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  static std::mutex mtx;
+  std::lock_guard<std::mutex> lk(mtx);
 
-  return {};
+  auto bm = block_allocator_->bm;
+  auto alloc_res = block_allocator_->allocate();
+  if (alloc_res.is_err()) {
+    return {0, 0};
+  }
+  auto bid = alloc_res.unwrap();
+  // zero newly allocated block
+  bm->zero_block(bid);
+
+  // Increase version stored in version block
+  const usize versions_per_block = bm->block_size() / sizeof(version_t);
+  const block_id_t ver_block_id = (bid / versions_per_block);
+  const usize ver_offset = (bid % versions_per_block) * sizeof(version_t);
+
+  std::vector<u8> ver_buf(bm->block_size());
+  bm->read_block(ver_block_id, ver_buf.data());
+  auto pver = reinterpret_cast<version_t *>(ver_buf.data() + ver_offset);
+  *pver = static_cast<version_t>(*pver + 1);
+  bm->write_block(ver_block_id, ver_buf.data());
+
+  return {bid, *pver};
 }
 
 // {Your code here}
 auto DataServer::free_block(block_id_t block_id) -> bool {
-  // TODO: Implement this function.
-  UNIMPLEMENTED();
+  static std::mutex mtx;
+  std::lock_guard<std::mutex> lk(mtx);
+  auto bm = block_allocator_->bm;
 
-  return false;
+  // zero the data block
+  bm->zero_block(block_id);
+  // bump version to invalidate stale mappings
+  const usize versions_per_block = bm->block_size() / sizeof(version_t);
+  const block_id_t ver_block_id = (block_id / versions_per_block);
+  const usize ver_offset = (block_id % versions_per_block) * sizeof(version_t);
+  std::vector<u8> ver_buf(bm->block_size());
+  bm->read_block(ver_block_id, ver_buf.data());
+  auto pver = reinterpret_cast<version_t *>(ver_buf.data() + ver_offset);
+  *pver = static_cast<version_t>(*pver + 1);
+  bm->write_block(ver_block_id, ver_buf.data());
+
+  // release from allocator
+  auto del = block_allocator_->deallocate(block_id);
+  return del.is_ok();
 }
 } // namespace chfs
